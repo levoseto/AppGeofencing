998757bcf315b0f3ce2618c146863414e727ed17
Marco Luna <Marco.Luna@Tideworks.com>
2022-07-05 10:21:03 -0500

    Se agregan servicios de Mensajes, y notificaciones en Android

---
+++ AppGeofencing/AppGeofencing/Services/GetLocationService.cs
@@ -0,0 +1,58 @@
+﻿using AppGeofencing.Models;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace AppGeofencing.Services
+{
+    public class GetLocationService
+    {
+        private readonly bool stopping = false;
+
+        public GetLocationService()
+        {
+        }
+
+        public async Task Run(CancellationToken token)
+        {
+            await Task.Run(async () =>
+            {
+                while (!stopping)
+                {
+                    token.ThrowIfCancellationRequested();
+                    try
+                    {
+                        await Task.Delay(2000);
+
+                        var request = new GeolocationRequest(GeolocationAccuracy.High);
+                        var location = await Geolocation.GetLocationAsync(request);
+                        if (location != null)
+                        {
+                            var message = new LocationMessage
+                            {
+                                Latitude = location.Latitude,
+                                Longitude = location.Longitude
+                            };
+
+                            MainThread.BeginInvokeOnMainThread(() =>
+                            {
+                                MessagingCenter.Send(message, "Location");
+                            });
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MainThread.BeginInvokeOnMainThread(() =>
+                        {
+                            var errormessage = new LocationErrorMessage();
+                            MessagingCenter.Send(errormessage, "LocationError");
+                        });
+                    }
+                }
+                return;
+            }, token);
+        }
+    }
+}
